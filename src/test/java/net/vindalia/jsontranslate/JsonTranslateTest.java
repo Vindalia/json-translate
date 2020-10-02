97daf017bc85a303e146e4557d2d1f15f5733343
Tyan <tyan@mail.de>
2020-10-02 21:13:52 +0200

    Added simple translation files

---
+++ src/test/java/net/vindalia/jsontranslate/JsonTranslateTest.java
@@ -0,0 +1,13 @@
+package net.vindalia.jsontranslate;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class JsonTranslateTest {
+
+    @Test
+    public void testSomeLibraryMethod() {
+        JsonTranslate classUnderTest = new JsonTranslate();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
