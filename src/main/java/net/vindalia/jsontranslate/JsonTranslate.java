41155c9eab4ae1e47cae7ce14723f35153130777
Tyan <tyan@mail.de>
2020-11-13 13:26:24 +0100

    Switched to JUnit 5 and added first poc

--- src/main/java/net/vindalia/jsontranslate/JsonTranslate.java
+++ src/main/java/net/vindalia/jsontranslate/JsonTranslate.java
@@ -1,8 +1,64 @@
 package net.vindalia.jsontranslate;
 
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JsonTranslate {
 
-    public boolean someLibraryMethod() {
-        return true;
+    private final HashMap<String, HashMap<String, String>> translationMap;
+
+    public JsonTranslate(File translationFile) throws IOException {
+        this.translationMap = new HashMap<>();
+        String json = this.readFromFile(translationFile);
+
+        JSONObject object = new JSONObject(json);
+        Map<String, Object> rawMap = object.toMap();
+        for (Map.Entry<String, Object> topLevelSet : rawMap.entrySet()) {
+            if (topLevelSet.getValue() instanceof String) {
+                this.insertSet("de", topLevelSet.getKey(), (String) topLevelSet.getValue());
+            } else if (topLevelSet.getValue() instanceof HashMap) {
+                this.processTranslations(topLevelSet.getKey(), (HashMap<String, Object>) topLevelSet.getValue());
+            }
+        }
+    }
+
+    public String translate(String language, String key) {
+        return this.translationMap.get(key).get(language);
+    }
+
+    private void processTranslations(String key, HashMap<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String translationKey = key + "." + entry.getKey();
+
+            if (entry.getValue() instanceof String) {
+                this.insertSet("de", translationKey, (String) entry.getValue());
+            } else if (entry.getValue() instanceof HashMap) {
+                this.processTranslations(translationKey, (HashMap<String, Object>) entry.getValue());
+            }
+        }
+    }
+
+    private void insertSet(String language, String key, String value) {
+        if (this.translationMap.containsKey(key)) {
+            this.translationMap.get(key).put(language, value);
+        } else {
+            HashMap<String, String> map = new HashMap<>();
+            map.put(language, value);
+            this.translationMap.put(key, map);
+        }
+    }
+
+    private String readFromFile(File translationFile) throws IOException {
+        StringBuilder resultBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(translationFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                resultBuilder.append(line).append("\n");
+            }
+        }
+        return resultBuilder.toString();
     }
 }
--- src/test/java/net/vindalia/jsontranslate/JsonTranslateTest.java
+++ src/test/java/net/vindalia/jsontranslate/JsonTranslateTest.java
@@ -1,13 +1,50 @@
 package net.vindalia.jsontranslate;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class JsonTranslateTest {
 
     @Test
-    public void testSomeLibraryMethod() {
-        JsonTranslate classUnderTest = new JsonTranslate();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    public void testJsonTranslateConstructor() {
+        File translationFile = new File("src/test/resources/i18n/de.json");
+        Assertions.assertDoesNotThrow(() -> {
+            new JsonTranslate(translationFile);
+        });
+    }
+
+    @Test
+    public void testItTranslatesRootLevel() {
+        JsonTranslate jsonTranslate = this.get();
+        String translation = jsonTranslate.translate("de", "ROOT_LEVEL");
+        Assertions.assertEquals("Haupt Level", translation);
+    }
+
+    @Test
+    public void testItTranslatesFirstLevel() {
+        JsonTranslate jsonTranslate = this.get();
+        String translation = jsonTranslate.translate("de", "FIRST_LEVEL.EXAMPLE");
+        Assertions.assertEquals("Beispiel auf erster Ebene", translation);
+    }
+
+    @Test
+    public void testItTranslatesSecondLevel() {
+        JsonTranslate jsonTranslate = this.get();
+        String translation = jsonTranslate.translate("de", "FIRST_LEVEL.SECOND_LEVEL.EXAMPLE");
+        Assertions.assertEquals("Beispiel auf zweiter Ebene", translation);
+    }
+
+    private JsonTranslate get() {
+        File translationFile = new File("src/test/resources/i18n/de.json");
+        JsonTranslate jsonTranslate = null;
+        try {
+            jsonTranslate = new JsonTranslate(translationFile);
+        } catch (IOException e) {
+            Assertions.fail("Could not create JsonTranslate", e);
+        }
+        return jsonTranslate;
     }
 }
